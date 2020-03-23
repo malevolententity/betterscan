d6c57e67565f6436a7905ed4f7795936c1fca8b8
HackerTupai <ronngjianying@gmail.com>
2020-03-23 03:38:42 -0400

    Minor edits. Final commit for forseeable future.

--- betterscan.py
+++ betterscan.py
@@ -1,6 +1,4 @@
 #!/usr/bin/env python 
-
-
 import sys
 import argparse
 import subprocess
@@ -9,33 +7,32 @@ import subprocess
 parser = argparse.ArgumentParser()
 #parser.add_argument("port_list", help="file containing ports scanned by massmap")
 parser.add_argument("ip", help="IP address to perform nmap on.")
-
 args = parser.parse_args()
 
 ################
 # MASSCAN PART #
 ################
-with open("massscan.txt", "w+") as f:
-	cmd = "sudo masscan -p1-65535,U:1-65535 " + args.ip + " --rate=1000 -e tun0"
-	subprocess.call([cmd], stdout=f, shell=True)
+with open("masscan.txt", "w+") as f:
+	cmd = "sudo masscan -p1-65535,U:1-65535 --rate=1000 --wait 0 -e tun0 " + args.ip
+	subprocess.call(cmd, stdout=f, shell=True)
 
 #############
 # NMAP PART #
 #############
 
 ports = []
-for line in open ("massscan.txt"):
+for line in open ("masscan.txt"):
 
 	#get the port/tcp string
 	port = line.split(" ")[3]
 	#remove /tcp and /udp from the string
 	port = port.split("/")[0]
 	ports.append(int(port))
-ports.sort()
-nmap_cmd = "nmap -A -T4 -p" + ",".join(map(str,ports)) + " " + args.ip
 
-p = subprocess.Popen(nmap_cmd, shell=True)
-p.wait()
+ports.sort()
+
+cmd = "nmap -A -T4 -v -p" + ",".join(map(str,ports)) + " " + args.ip
+subprocess.call(cmd, shell=True)
 
 
 
